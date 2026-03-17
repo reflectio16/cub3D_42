a4452604365cff21752cd1aeab051ea33ae922e6
Merve Elma <meelma@student.42.fr>
2026-03-17 14:34:00 +0100

    Remove M_PI define

--- include/defines.h
+++ include/defines.h
@@ -3,17 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   defines.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fmoulin <fmoulin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: meelma <meelma@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/14 15:00:00 by meelma            #+#    #+#             */
-/*   Updated: 2026/03/16 16:08:36 by fmoulin          ###   ########.fr       */
+/*   Updated: 2026/03/17 14:32:06 by meelma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DEFINES_H
 # define DEFINES_H
 
-# define M_PI 3.14159265358979323846
 # define WIDTH 1920
 # define HEIGHT 1080
 # define BLUE 0x5E00FF
